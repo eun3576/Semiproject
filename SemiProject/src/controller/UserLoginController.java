ccbb5d937af65493d48ee968af97b39c3ce77d84
eun3576 <99810971+eun3576@users.noreply.github.com>
2022-03-15 22:58:19 +0900

    Add files via upload

---
+++ SemiProject/src/controller/UserLoginController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import dto.UserInfo;
+import service.face.UserLoginService;
+import service.impl.UserLoginServiceImpl;
+
+@WebServlet("/user/login")
+public class UserLoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserLoginService userLoginService = new UserLoginServiceImpl();
+
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		System.out.println("/user/login[post]");
+		String uid = req.getParameter("userid");
+		String upw = req.getParameter("userpw");
+		
+		UserInfo user = new UserInfo();
+		user.setId(uid);
+		user.setPassword(upw);
+		int logincnt = userLoginService.login(user);
+//		System.out.println(logincnt);
+		//세션 정보 처리
+		if( logincnt > 0 ) { //로그인 인증 성공
+			
+//			System.out.println(user);
+			user = userLoginService.info(user);
+//			System.out.println(user);
+			
+			//세션정보 저장하기
+			HttpSession session = req.getSession();
+			
+			session.setAttribute("login", true);
+			session.setAttribute("userid", user.getId());
+			session.setAttribute("usernick", user.getNickname());
+		}
+		
+			//메인페이지로 리다이렉트
+			resp.sendRedirect("/");
+		
+	}
+
+}
