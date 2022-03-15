2fb9737b733d6f0e882d234ec8d9e47d8b7d4399
jjw <jjjinoo@naver.com>
2022-03-15 10:20:02 +0900

    mvc 기본 설정

---
+++ SemiProject/src/common/JDBCTemplate.java
@@ -0,0 +1,100 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTemplate {
+	//OJDBC 드라이버
+	private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	
+	//DB연결 정보
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USERNAME = "scott";
+	private static final String PASSWORD = "tiger";
+	
+	//private 생성자
+	private JDBCTemplate() {}
+	
+	//DB연결 객체
+	private static Connection conn;
+	
+	public static Connection getConnection() {
+		
+		try {
+			//드라이버 로드
+			Class.forName(DRIVER);
+			
+			//DB 연결
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			
+			//AutoCommit 설정 끄기
+			conn.setAutoCommit(false);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;		
+	}
+	
+	//Connection 객체 닫기
+	public static void close(Connection conn) {
+		try {
+			if(conn!=null && !conn.isClosed()) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Statement 객체 닫기
+	public static void close(Statement st) {
+		try {
+			if(st!=null && !st.isClosed()) st.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//PreparedStatement 객체 닫기
+	public static void close(PreparedStatement ps) {
+		try {
+			if(ps!=null && !ps.isClosed()) ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//ResultSet 객체 닫기
+	public static void close(ResultSet rs) {
+		try {
+			if(rs!=null && !rs.isClosed()) rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//COMMIT 수행
+	public static void commit(Connection conn) {
+		try {
+			if(conn!=null && !conn.isClosed()) conn.commit();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//ROLLBACK 수행
+	public static void rollback(Connection conn) {
+		try {
+			if(conn!=null && !conn.isClosed()) conn.rollback();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
