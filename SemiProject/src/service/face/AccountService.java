be237130f92a3194e9b6de8d80e708cabb474ef1
eun3576 <99810971+eun3576@users.noreply.github.com>
2022-03-17 15:30:59 +0900

    Update AccountService.java

--- SemiProject/src/service/face/AccountService.java
+++ SemiProject/src/service/face/AccountService.java
@@ -1,5 +1,7 @@
 package service.face;
 
+import javax.servlet.http.HttpServletRequest;
+
 import dto.UserInfo;
 
 public interface AccountService {
@@ -17,6 +19,35 @@ public interface AccountService {
 	 * @return 회원삽입결과
 	 */
 	public int insertUser(UserInfo user);
+
+	/**
+	 * 아이디를 매개변수로 받아 저장한다
+	 * @param req 객체에 저장할 매개변수
+	 * @return 저장한 user객체
+	 */
+	public UserInfo getUserJoinId(HttpServletRequest req);
+
+	/**
+	 * 회원가입시 기존의 아이디인지 확인한다
+	 * @param user 확인할 아이디
+	 * @return 기존 아이디일시 화면에 표시할 String
+	 */
+	public String userIdCheck(UserInfo user);
+
+	/**
+	 * 닉네임을 메개변수로 받아 저장한다
+	 * @param req 객체에 저장할 매개변수
+	 * @return 저장한 user객체
+	 */
+	public UserInfo getUserJoinNick(HttpServletRequest req);
+
+	/**
+	 * 회원가입시 기존의 닉네임인지 확인한다
+	 * @param user 확인할 닉네임
+	 * @return 기존 닉네임일시 화면에 표시할 String
+	 */
+	public String userNickCheck(UserInfo user);
+
 
 
 }
