8e959e1eae47b6631f500e0b52373cdd5381b777
eun3576 <99810971+eun3576@users.noreply.github.com>
2022-03-18 16:41:19 +0900

    Update MainService.java

--- SemiProject/src/service/face/MainService.java
+++ SemiProject/src/service/face/MainService.java
@@ -2,6 +2,7 @@ package service.face;
 
 import java.util.List;
 
+import dto.Product;
 import dto.ProductTag;
 import dto.Review;
 
@@ -18,5 +19,19 @@ public interface MainService {
 	 * @return 조회된 리뷰 목록
 	 */
 	public List<Review> getReviewList();
+
+	/**
+	 * 검색어 파라미터를 단어별로 나눠 배열로 받는다
+	 * @param search 배열로 만들 String 
+	 * @return String 배열
+	 */
+	public String[] divideSearch(String search);
+
+	/**
+	 * 배열로 받은 검색어로 상품을 검색한다
+	 * @param searchItems 검색할 배열
+	 * @return 해당하는 상품 리스트
+	 */
+	public List<Product> searchBysearchItems(String[] searchItems);
 
 }
