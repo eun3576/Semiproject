a13e46197b7eca9c5b51d3c3be89e6bc660d2178
eun3576 <99810971+eun3576@users.noreply.github.com>
2022-03-18 16:42:54 +0900

    Update MainDao.java

--- SemiProject/src/dao/face/MainDao.java
+++ SemiProject/src/dao/face/MainDao.java
@@ -3,6 +3,7 @@ package dao.face;
 import java.sql.Connection;
 import java.util.List;
 
+import dto.Product;
 import dto.ProductTag;
 import dto.Review;
 
@@ -21,5 +22,13 @@ public interface MainDao {
 	 * @return 조회된 리뷰 목록
 	 */
 	public List<Review> selectReviewByViews(Connection conn);
+
+	/**
+	 * 검색어에 해당하는 상품 list가져오기
+	 * @param conn db연결 객체
+	 * @param searchItems 검색어(들)
+	 * @return 검색어에 해당하는 객체
+	 */
+	public List<Product> selectBySearchItems(Connection conn, String[] searchItems);
 
 }
