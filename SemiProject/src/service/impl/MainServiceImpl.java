a38a099e8495b4b23418d05267fd0702d8012e4d
eun3576 <99810971+eun3576@users.noreply.github.com>
2022-03-18 16:42:14 +0900

    Update MainServiceImpl.java

--- SemiProject/src/service/impl/MainServiceImpl.java
+++ SemiProject/src/service/impl/MainServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import common.JDBCTemplate;
 import dao.face.MainDao;
 import dao.impl.MainDaoImpl;
+import dto.Product;
 import dto.ProductTag;
 import dto.Review;
 import service.face.MainService;
@@ -28,6 +29,25 @@ public class MainServiceImpl implements MainService{
 	public List<Review> getReviewList() {
 		Connection conn = JDBCTemplate.getConnection();
 		return mainDao.selectReviewByViews(conn);
+	}
+
+
+	@Override
+	public String[] divideSearch(String search) {
+		
+		//문자열을 소문자로 변경한다
+		search = search.toLowerCase();
+		//문자열을 공백으로 나눈다
+		String[] strs = search.split(" ");
+		return strs;
+	}
+
+
+	@Override
+	public List<Product> searchBysearchItems(String[] searchItems) {
+		Connection conn = JDBCTemplate.getConnection();
+		List<Product> pList = mainDao.selectBySearchItems(conn, searchItems);
+		return pList;
 	}
 
 }
