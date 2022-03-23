b7fb1ca07b04ad707243c8fb98a6b5d9242d2b99
eun3576 <99810971+eun3576@users.noreply.github.com>
2022-03-23 19:38:10 +0900

    Update MainService.java

--- SemiProject/src/service/face/MainService.java
+++ SemiProject/src/service/face/MainService.java
@@ -12,7 +12,7 @@ public interface MainService {
 	 * 베스트 상품 목록 조회
 	 * @return 조회된 상품 목록
 	 */
-	public List<ProductTag> getBestList();
+	public List<Product> getBestList();
 
 	/**
 	 * 조회수가 높은 리뷰 조회
